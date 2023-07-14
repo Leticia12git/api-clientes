b6b487582a06fb53f987febbcc8bb4e9929942e3
Leticia12 <leticia.silva@arphoenix.com.br>
2023-07-14 09:54:09 -0300

    Adicionando arquivo aplicatio properties de producao no projeto

---
+++ src/test/java/br/com/projeto/APiClientes/controllers/ClienteControllerTest.java
@@ -0,0 +1,31 @@
+package br.com.projeto.APiClientes.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ClienteControllerTest {
+
+    @Test
+    @WithMockUser
+    void listarClientes() {
+    }
+
+    @Test
+    void buscarPoId() {
+    }
+
+    @Test
+    void cadastrar() {
+    }
+
+    @Test
+    void atualizar() {
+    }
+
+    @Test
+    void deletar() {
+    }
+}
